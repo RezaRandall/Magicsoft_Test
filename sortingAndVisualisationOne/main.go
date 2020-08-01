f81d056699906cc84507dd191d2e096a6486d43a
RezaRandall <dwirezairawan07@gmail.com>
2020-08-01 15:24:13 +0700

    Recommit sorting test no 3, fix error undefined variable and rewrite code for more concise code

--- sortingAndVisualisationOne/main.go
+++ sortingAndVisualisationOne/main.go
@@ -8,103 +8,19 @@ import (
 
 func main() {
 	arrNumber := []int{1, 4, 5, 6, 8, 2}
-	max := math.MinInt64
+
+	// Vertical bar chart
 	barChartVerticaly(arrNumber)
 	fmt.Println()
 
 	// Insertion sort ascending
 	fmt.Println()
 	insertionSortAsc(arrNumber)
-
-	// Find the maximum to determine the suitable grid to plot the Histogram
-	for _, e := range arrNumber {
-		if e > max {
-			max = e
-		}
-	}
-	var (
-		rows  = max
-		colmn = len(arrNumber)
-	)
-
-	// Create grind of dimension (row * colmn)
-	// Allocate rows
-	histograms := make([][]string, rows)
-	for i := 0; i < rows; i++ {
-		// Allocate column for each row
-		histograms[i] = make([]string, colmn)
-	}
-
-	// Histogram formation logic
-	// use blank spaces ("	") or "  |  " to fill the grid
-	for i, e := range arrNumber {
-		for j := rows - 1; j >= 0; j-- {
-			if j >= rows-e {
-				histograms[j][i] = " | "
-			} else {
-				histograms[j][i] = "   "
-			}
-		}
-	}
-
-	// Print the histogram
-	for i := 0; i < rows; i++ {
-		for j := 0; j < colmn; j++ {
-			fmt.Printf("%s", histograms[i][j])
-		}
-		fmt.Println()
-	}
-
-	for i := 0; i < colmn; i++ {
-		fmt.Printf(" %d ", arrNumber[i])
-	}
 	fmt.Println()
 
 	// Insertion sort Desc
 	fmt.Println()
 	insertionSortDesc(arrNumber)
-	// Find the maximum to determine the suitable grid to plot the Histogram
-	for _, e := range arrNumber {
-		if e > max {
-			max = e
-		}
-	}
-	var (
-		baris = max
-		kolom = len(arrNumber)
-	)
-
-	// Create grind of dimension (row * colmn)
-	// Allocate rows
-	histogramss := make([][]string, rows)
-	for i := 0; i < baris; i++ {
-		// Allocate column for each row
-		histogramss[i] = make([]string, kolom)
-	}
-
-	// Histogram formation logic
-	// use blank spaces ("	") or "  |  " to fill the grid
-	for i, e := range arrNumber {
-		for j := baris - 1; j >= 0; j-- {
-			if j >= baris-e {
-				histogramss[j][i] = " | "
-			} else {
-				histogramss[j][i] = "   "
-			}
-		}
-	}
-
-	// Print the histogram
-	for i := 0; i < baris; i++ {
-		for j := 0; j < kolom; j++ {
-			fmt.Printf("%s", histogramss[i][j])
-		}
-		fmt.Println()
-	}
-
-	for i := 0; i < kolom; i++ {
-		fmt.Printf(" %d ", arrNumber[i])
-	}
 
 }
 
@@ -164,8 +80,10 @@ func insertionSortAsc(arrNum []int) {
 		}
 		arrNum[j] = tmp
 	}
+	barChartVerticaly(arrNum)
 }
 
 func insertionSortDesc(arrNumber []int) {
 	sort.Sort(sort.Reverse(sort.IntSlice(arrNumber)))
+	barChartVerticaly(arrNumber)
 }
