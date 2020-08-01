ddbc631433a3606769e3d7b94a1627bb181ddd51
RezaRandall <dwirezairawan07@gmail.com>
2020-08-01 13:53:37 +0700

    Commit sorting test no 2, ascending array using insertion sort algorithm

--- sortingAndVisualisationOne/main.go
+++ sortingAndVisualisationOne/main.go
@@ -8,8 +8,61 @@ import (
 func main() {
 	var arrNumber = []int{1, 4, 5, 6, 8, 2}
 	max := math.MinInt64
+	barChartVerticaly(arrNumber)
+	fmt.Println()
+
+	// Insertion sort ascending
+	fmt.Println()
+	insertionSort(arrNumber)
 
 	// Find the maximum to determine the suitable grid to plot the Histogram
+	for _, e := range arrNumber {
+		if e > max {
+			max = e
+		}
+	}
+	var (
+		rows  = max
+		colmn = len(arrNumber)
+	)
+
+	// Create grind of dimension (row * colmn)
+	// Allocate rows
+	histograms := make([][]string, rows)
+	for i := 0; i < rows; i++ {
+		// Allocate column for each row
+		histograms[i] = make([]string, colmn)
+	}
+
+	// Histogram formation logic
+	// use blank spaces ("	") or "  |  " to fill the grid
+	for i, e := range arrNumber {
+		for j := rows - 1; j >= 0; j-- {
+			if j >= rows-e {
+				histograms[j][i] = " | "
+			} else {
+				histograms[j][i] = "   "
+			}
+		}
+	}
+
+	// Print the histogram
+	for i := 0; i < rows; i++ {
+		for j := 0; j < colmn; j++ {
+			fmt.Printf("%s", histograms[i][j])
+		}
+		fmt.Println()
+	}
+
+	for i := 0; i < colmn; i++ {
+		fmt.Printf(" %d ", arrNumber[i])
+	}
+
+}
+
+func barChartVerticaly(arrNumber []int) {
+	// Find the maximum to determine the suitable grid to plot the Histogram
+	max := math.MinInt64
 	for _, e := range arrNumber {
 		if e > max {
 			max = e
@@ -51,5 +104,16 @@ func main() {
 	for i := 0; i < col; i++ {
 		fmt.Printf(" %d ", arrNumber[i])
 	}
-	fmt.Println()
+}
+
+func insertionSort(arrNum []int) {
+	for i := 0; i < len(arrNum); i++ {
+		tmp := arrNum[i]
+		j := i
+		for j > 0 && arrNum[j-1] > tmp {
+			arrNum[j] = arrNum[j-1]
+			j--
+		}
+		arrNum[j] = tmp
+	}
 }
